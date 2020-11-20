91ac4680fd3beaf701d9130f2c268d0dd3f851e2
shrutishankar28 <shrutishankar28@gmail.com>
2020-11-20 20:41:11 +0530

    Added designs

--- App.js
+++ App.js
@@ -6,108 +6,105 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  Dimensions,
+  Platform,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const {width: deviceWidth} = Dimensions.get('window');
 
-const App: () => React$Node = () => {
+const colors = {
+  background: '#06151C',
+  lightGray: '#F4F5FB',
+};
+
+const spacing = [0, 5, 10, 15, 20, 25, 30, 40, 50];
+
+const App = () => {
+  useEffect(() => {
+    Platform.OS === 'android' &&
+      StatusBar.setBackgroundColor(colors.background);
+  }, []);
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.body}>
+        <View style={styles.whiteBox}>
+          <View style={styles.leftView} />
+          <View style={styles.upperBox}>
+            <View style={styles.blackBox} />
+            <Text style={styles.sectionText}>Name</Text>
+            <View style={styles.blackBox} />
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          <View style={styles.lowerBox}>
+            <Text style={styles.sectionText}>Balance</Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
   },
   body: {
-    backgroundColor: Colors.white,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  whiteBox: {
+    height: 200,
+    width: deviceWidth * 0.7,
+    backgroundColor: 'white',
+    borderRadius: spacing[4],
+    overflow: 'hidden',
+    position: 'relative',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  leftView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: spacing[5],
+    height: 200,
+    backgroundColor: colors.lightGray,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  upperBox: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderBottomLeftRadius: spacing[6],
+    paddingHorizontal: spacing[3],
+    paddingVertical: spacing[5],
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  blackBox: {
+    borderRadius: 10,
+    height: 45,
+    width: 45,
+    backgroundColor: colors.background,
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  sectionText: {
+    fontSize: 20,
+  },
+  lowerBox: {
+    flex: 1,
+    backgroundColor: colors.lightGray,
+    paddingVertical: spacing[5],
+    borderTopRightRadius: spacing[6],
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
